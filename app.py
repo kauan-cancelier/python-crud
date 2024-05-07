15c4ed997d24f7cac47a369df944e3faf8ea766b
Julio Henrique <102197851+juliohenriquec@users.noreply.github.com>
2024-05-07 20:32:37 -0300

    Criada função de adicionar

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
 # initialization
+class Pessoa:
+  def __init__(self, nome, sobrenome, email, telefone):
+    self.nome = nome
+    self.sobrenome = sobrenome
+    self.email = email
+    self.telefone = telefone
 
-print('hello world')
-print('oi')
+
+
+
+pessoas = []
+
+
+
+
+menu =  " 1 - Adicionar\n 2 - Visualizar\n 3 - Editar\n 4 - Sair"
+
+print('Menu de Opções')
+print(menu)
+
+opcao = 0
+
+while opcao != 4:
+    opcao = int(input("Escolha uma opção: "))
+    if opcao == 1:
+        nome = input('Nome: ')
+        sobrenome = input('Sobrenome: ')
+        email = input('E-mail: ')
+        telefone = input('Telefone: ')
+        p1 = Pessoa(nome, sobrenome, email, telefone)
+        pessoas.append(p1)
+        print('Pessoa adicionada com SUCESSO')
+
+    print(menu)
