6a9d939f4d45bf2d794ffe6180610bf4c63a30f6
SOFIA DELFINO RAULINO <sofia_raulino@estudante.sesisenai.org.br>
2024-05-07 21:21:55 -0300

    adicionado a opção visualizar

--- app.py
+++ app.py
@@ -6,13 +6,7 @@ class Pessoa:
     self.email = email
     self.telefone = telefone
 
-
-
-
 pessoas = []
-
-
-
 
 menu =  " 1 - Adicionar\n 2 - Visualizar\n 3 - Editar\n 4 - Sair"
 
@@ -32,4 +26,11 @@ while opcao != 4:
         pessoas.append(p1)
         print('Pessoa adicionada com SUCESSO')
 
+    elif opcao == 2:
+       for c in range (0,len(pessoas)):
+          p = pessoas[c]
+          print(f"\n cod: {c}\n nome: {p.nome}\n sobrenome: {p.sobrenome}\n email: {p.email}\n telefone: {p.telefone}\n")
+          
     print(menu)
+
+    
