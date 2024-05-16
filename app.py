e6841b8720e1d6ffec5e7d33b1a297772da9b9af
Eduarda Manenti <eduarda.manenti@nuntec.com.br>
2024-05-16 09:41:36 -0300

    Implementado lógica de gerenciamento de agendas.

--- app.py
+++ app.py
@@ -1,69 +1,89 @@
-# initialization
-class Pessoa:
-  def __init__(self, nome, sobrenome, email, telefone):
-    self.nome = nome
-    self.sobrenome = sobrenome
-    self.email = email
-    self.telefone = telefone
+class Contato:
+    def __init__(self):
+        self.id = 0
+        self.nome_completo = ''
+        self.email = ''
+        self.numero = ''
 
-pessoas = []
+    def set_nome_completo(self, nome_completo: str):
+        while not nome_completo.strip():
+            print("O nome completo não pode estar vazio.")
+            nome_completo = input('Digite o nome completo: ')
+        self.nome_completo = nome_completo
 
-menu =  " 1 - Adicionar\n 2 - Visualizar\n 3 - Editar\n 4 - Sair"
+    def set_email(self, email: str):
+        while not email.strip():
+            print("O e-mail não pode estar vazio.")
+            email = input('Digite o e-mail: ')
+        self.email = email
 
-print('Menu de Opções')
-print(menu)
+    def set_numero(self, numero: str):
+        while not numero.strip():
+            print("O número de telefone não pode estar vazio.")
+            numero = input('Digite o número de telefone: ')
+        self.numero = numero
 
-opcao = 0
+    def exibir_contato(self):
+        print(f'\nCódigo: {self.id} \n  Nome completo: {self.nome_completo}\n  Telefone: {self.numero}\n')
 
-while opcao != 4:
-    opcao = int(input("Escolha uma opção: "))
-    if opcao == 1:
-        nome = input('Nome: ')
-        tamanho_nome = nome.strip()
-        while len(tamanho_nome) <=0:
-            print("\n Digite um nome: ")
-            nome = input('Nome: ')
-            tamanho_nome = nome.strip()
+contatos = []
 
-        sobrenome = input('Sobrenome: ')
-        tamanho_sobrenome = sobrenome.strip()
-        while len(tamanho_sobrenome) <=0:
-            print("\n Digite um sobrenome: ")
-            sobrenome= input('Sobrenome: ')
-            tamanho_sobrenome = sobrenome.strip()
+opcao_escolhida = 0
 
-        email = input('E-mail: ')
-        tamanho_email = email.strip()
-        while len(tamanho_email) <=0:
-            print("\n Digite um e-mail: ")
-            email = input('E-mail: ')
-            tamanho_email = email.strip()
+def formulario(contato: Contato):
+    contato.set_nome_completo(input('Digite o nome completo: '))
+    contato.set_email(input('Digite o e-mail: '))
+    contato.set_numero(input('Digite o número de telefone: '))
+    return contato
 
-        telefone = input('Telefone: ')
-        tamanho_telefone = telefone.strip()
-        while len(tamanho_telefone) <=0:
-            print("\n Digite um telefone: ")
-            telefone = input('Telefone: ')
-            tamanho_telefone = telefone.strip()
+def inserir():
+    p = Contato()
+    p = formulario(p)
+    p.id = len(contatos) + 1
+    contatos.append(p)
 
-        p1 = Pessoa(nome, sobrenome, email, telefone)
-        pessoas.append(p1)
-        print('Pessoa adicionada com SUCESSO')
+def mostrar_contato():
+    if len(contatos) == 0:
+        print("\nNenhuma contato cadastrado.\n")
+    else:
+        print('\nLista de contatos\n')
+        for p in contatos:
+            p.exibir_contato()
 
-    elif opcao == 2:
-       for c in range (0,len(pessoas)):
-          p = pessoas[c]
-          print(f"\n cod: {c}\n nome: {p.nome}\n sobrenome: {p.sobrenome}\n email: {p.email}\n telefone: {p.telefone}\n")
-          
-    elif opcao == 3:
-        escolhida = int(input('informe aqui a pessoa a ser editada: '))
-        p= pessoas[escolhida]
-        p.nome = input('Nome: ')
-        p.sobrenome = input('Sobrenome: ')
-        p.email = input('E-mail: ')
-        p.telefone = input('Telefone: ')
-        print(pessoas[escolhida].nome)
+def editar(contato: Contato):
+    formulario(contato)
 
-    print(menu)
+def menu():
+    print('\n######################')
+    print('Menu')
+    print(f'1 - Novo contato \n2 - Ver contatos \n3 - Editar contato \n4 - Encerrar')
+    print('######################')
+    try:
+        return int(input('Escolha uma opção do menu: '))
+    except:
+        print('Digite uma opção válida. ')
 
-    
+while opcao_escolhida != 4:
+    opcao_escolhida = menu()
+    if opcao_escolhida == 1:
+        inserir()
+        print('Contato cadastrado com sucesso!')
+    elif opcao_escolhida == 2:
+        mostrar_contato()
+    elif opcao_escolhida == 3:
+        if len(contatos) == 0:
+            print("Não há contatos para editar.")
+        else:
+            mostrar_contato()
+            quem_editar = int(input('Digite o código da contato que deseja editar: '))
+            if quem_editar <= 0 or quem_editar > len(contatos):
+                print("Código de contato inválido.")
+            else:
+                contato_editar = contatos[quem_editar - 1]
+                editar(contato_editar)
+                print('Contato editado com sucesso!')
+
+
+
+print('Saindo...')
+
